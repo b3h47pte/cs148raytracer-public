a0098326320de844b190734a9daf9e4907ed6da5
Michael Bao <halo343gs@gmail.com>
2016-11-08 10:48:31 -0800

    prep for assignment 7

--- assignment7/Assignment7.cpp
+++ assignment7/Assignment7.cpp
@@ -18,26 +18,54 @@ std::shared_ptr<Scene> Assignment7::CreateScene() const
     std::shared_ptr<BlinnPhongMaterial> cubeMaterial = std::make_shared<BlinnPhongMaterial>();
     cubeMaterial->SetDiffuse(glm::vec3(1.f, 1.f, 1.f));
     cubeMaterial->SetSpecular(glm::vec3(0.6f, 0.6f, 0.6f), 40.f);
+    cubeMaterial->SetReflectivity(0.3f);
 
     // Objects
     std::vector<std::shared_ptr<aiMaterial>> loadedMaterials;
-    std::vector<std::shared_ptr<MeshObject>> cubeObjects = MeshLoader::LoadMesh("CornellBox/CornellBox-Photon.obj", &loadedMaterials);
+    std::vector<std::shared_ptr<MeshObject>> cubeObjects = MeshLoader::LoadMesh("CornellBox/CornellBox-Assignment7-Alt.obj", &loadedMaterials);
     for (size_t i = 0; i < cubeObjects.size(); ++i) {
         std::shared_ptr<Material> materialCopy = cubeMaterial->Clone();
         materialCopy->LoadMaterialFromAssimp(loadedMaterials[i]);
         cubeObjects[i]->SetMaterial(materialCopy);
-    }
 
-    std::shared_ptr<SceneObject> cubeSceneObject = std::make_shared<SceneObject>();
-    cubeSceneObject->AddMeshObject(cubeObjects);
-    cubeSceneObject->Rotate(glm::vec3(1.f, 0.f, 0.f), PI / 2.f);
-    cubeSceneObject->CreateAccelerationData(AccelerationTypes::BVH);
-    newScene->AddSceneObject(cubeSceneObject);
+        std::shared_ptr<SceneObject> cubeSceneObject = std::make_shared<SceneObject>();
+        cubeSceneObject->AddMeshObject(cubeObjects[i]);
+        cubeSceneObject->Rotate(glm::vec3(1.f, 0.f, 0.f), PI / 2.f);
+        cubeSceneObject->CreateAccelerationData(AccelerationTypes::BVH);
+
+        cubeSceneObject->ConfigureAccelerationStructure([](AccelerationStructure* genericAccelerator) {
+            BVHAcceleration* accelerator = dynamic_cast<BVHAcceleration*>(genericAccelerator);
+            accelerator->SetMaximumChildren(2);
+            accelerator->SetNodesOnLeaves(2);
+        });
+
+        cubeSceneObject->ConfigureChildMeshAccelerationStructure([](AccelerationStructure* genericAccelerator) {
+            BVHAcceleration* accelerator = dynamic_cast<BVHAcceleration*>(genericAccelerator);
+            accelerator->SetMaximumChildren(2);
+            accelerator->SetNodesOnLeaves(2);
+        });
+
+
+        newScene->AddSceneObject(cubeSceneObject);
+    }
 
     // Lights
     std::shared_ptr<Light> pointLight = std::make_shared<PointLight>();
-    pointLight->SetPosition(glm::vec3(0.01909f, 0.0101f, 1.77640f));
+    pointLight->SetPosition(glm::vec3(0.01909f, 0.0101f, 1.97028f));
     pointLight->SetLightColor(glm::vec3(1.f, 1.f, 1.f));
+
+// Assignment 7 Part 1 TODO: Change the '1' here.
+#define ACCELERATION_TYPE 1
+#if ACCELERATION_TYPE == 0
+    newScene->GenerateAccelerationData(AccelerationTypes::NONE);
+#elif ACCELERATION_TYPE == 1
+    newScene->GenerateAccelerationData(AccelerationTypes::BVH);
+#else
+    UniformGridAcceleration* accelerator = dynamic_cast<UniformGridAcceleration*>(newScene->GenerateAccelerationData(AccelerationTypes::UNIFORM_GRID));
+    assert(accelerator);
+    // Assignment 7 Part 2 TODO: Change the glm::ivec3(10, 10, 10) here.
+    accelerator->SetSuggestedGridSize(glm::ivec3(10, 10, 10));
+#endif    
     newScene->AddLight(pointLight);
 
     return newScene;
@@ -52,7 +80,7 @@ std::shared_ptr<ColorSampler> Assignment7::CreateSampler() const
 
 std::shared_ptr<class Renderer> Assignment7::CreateRenderer(std::shared_ptr<Scene> scene, std::shared_ptr<ColorSampler> sampler) const
 {
-    return std::make_shared<PhotonMappingRenderer>(scene, sampler);
+    return std::make_shared<BackwardRenderer>(scene, sampler);
 }
 
 int Assignment7::GetSamplesPerPixel() const
@@ -67,12 +95,12 @@ bool Assignment7::NotifyNewPixelSample(glm::vec3 inputSampleColor, int sampleInd
 
 int Assignment7::GetMaxReflectionBounces() const
 {
-    return 0;
+    return 2;
 }
 
 int Assignment7::GetMaxRefractionBounces() const
 {
-    return 0;
+    return 4;
 }
 
 glm::vec2 Assignment7::GetImageOutputResolution() const
